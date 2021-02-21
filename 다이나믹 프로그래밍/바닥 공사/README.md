b12e970bbbffc4fb76d0b26646d2cf082551b4dc
ssudev <goodys1011@naver.com>
2021-02-21 22:05:54 +0900

    바닥 공사

--- 다이나믹 프로그래밍/바닥 공사/README.md
+++ 다이나믹 프로그래밍/바닥 공사/README.md
@@ -4,9 +4,7 @@
 </div>
 
 ### 문제 해설
-- 바텀업방식으로 문제를 해결한다.
-- 점화식은 dp[n] = min(a,b,c,d) + 1
-    1. a = -1
-    2. b = /2
-    3. c = /3
-    4. d = /5
+- 점화식은 dp(n) = dp(n-1) + dp(n-2)*2
+<div>
+    <img src="image2.PNG" width="400" height="500">
+</div>
--- 다이나믹 프로그래밍/바닥 공사/solution.py
+++ 다이나믹 프로그래밍/바닥 공사/solution.py
@@ -1,25 +1,11 @@
-import sys
-
 x = int(input())
-dp = [0]*(x+1)
 
-dp[1] = 0
+dp = [0] * (x+1)
 
+dp[1] = 1
+dp[2] = 3
 
-for i in range(2, x+1):
-    a,b,c,d = sys.maxsize,sys.maxsize,sys.maxsize,sys.maxsize
+for i in range(3,x+1):
+    dp[i] = dp[i-1] + dp[i-2] * 2
 
-    if i % 5 == 0:
-        a = dp[int(i/5)] + 1
-    
-    if i % 3 == 0:
-        b = dp[int(i/3)] + 1
-    
-    if i % 2 == 0:
-        c = dp[int(i/2)] + 1
-    
-    d = dp[i-1] + 1
-
-    dp[i] = min(a,b,c,d)
-
-print(dp)
+print(dp[x])
