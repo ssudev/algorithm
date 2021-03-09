e6a302784abd78f7d2799ee819589f3b80bce117
ssudev <goodys1011@naver.com>
2021-03-09 23:16:25 +0900

    문자열 뒤집기 수정

--- 실전 문제 유형/그리디 문제/문자열 뒤집기/solution.py
+++ 실전 문제 유형/그리디 문제/문자열 뒤집기/solution.py
@@ -2,24 +2,19 @@ n = input()
 
 cnt_0 = 0
 cnt_1 = 0
-reverse_s = ""
 
-for s in n:
-    if s == "0":
+for i in range(len(n)-1):
+
+    if n[i] != n[i+1] and n[i] == "0":
         cnt_0 += 1
-    else:
+    elif n[i] != n[i+1] and n[i] == "1":
         cnt_1 += 1
 
-if cnt_0 >= cnt_1:
-    reverse_s = "1"
-else:
-    reverse_s = "0"
+if len(n) > 1:
 
-result = 0
+    if n[-1] == "0" and n[-2] == "1":
+        cnt_0 += 1
+    elif n[-1] == "1" and n[-2] == "0":
+        cnt_1 += 1
 
-for i in range(1,len(n)):
-    
-    if n[i] == reverse_s and n[i-1] != reverse_s:
-        result += 1 
-
-print(result)
+print(min(cnt_0, cnt_1))
