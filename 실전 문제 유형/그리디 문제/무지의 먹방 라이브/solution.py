8571980ab2f99ec4991ac5ad6c52efebfbaf5643
ssudev <goodys1011@naver.com>
2021-03-15 21:33:04 +0900

    무지의 먹방 라이브

--- 실전 문제 유형/그리디 문제/무지의 먹방 라이브/solution.py
+++ 실전 문제 유형/그리디 문제/무지의 먹방 라이브/solution.py
@@ -1,36 +1,29 @@
+import heapq
+
 def solution(food_times, k):
-    answer = 0
-    index = -1
+    q = []
     
-    for i in range(1, k+2):
-        index += 1
+    for i in range(len(food_times)):
+        heapq.heappush(q, [food_times[i], i+1])
+        
+    pre_time = 0
+    now_time = 0
+    sum_time = 0
+    n = len(food_times)
     
-        if index == len(food_times):
-            index = 0
+    while k - (sum_time + (q[0][0] - pre_time)*n) >= 0:
+        now_time, index = heapq.heappop(q)
+        sum_time += (now_time-pre_time) * n
+        pre_time = now_time
+        n -= 1
 
-        if food_times[index] != 0:
-            food_times[index] -= 1
-        elif food_times[index] == 0:
-            temp_index = index
-            
-            while True:
-                index += 1
-                if index == len(food_times):
-                    index = 0
-                    
-                if index == temp_index:
-                    index = -1
-                    break
-                
-                if food_times[index] != 0:
-                    food_times[index] -= 1
-                    break
-        if index == -1:
-            answer = -1
-            return answer
-            
-    answer = index + 1
+        print(now_time, sum_time, pre_time, n)
     
-    return answer
+        if not q:
+            return -1
+    
+    result = sorted(q, key=lambda x:x[1])
+    
+    return result[(k-sum_time)%len(q)][1]
 
-print(solution([4,1,1,5],4))
+print(solution([3,1,2], 5))
